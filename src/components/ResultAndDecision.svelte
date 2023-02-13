d4361c430b8d79085c72945d7b3ece16689eb08d
Vinicius Akira Imaizumi <daquisu@gmail.com>
2023-02-13 22:09:18 +0100

    Add event forwarding for decision

--- src/components/ResultAndDecision.svelte
+++ src/components/ResultAndDecision.svelte
@@ -1,14 +1,22 @@
 <script>
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
     function getRandomIntBetweenZeroAndOneHundred() {
 		return Math.floor(Math.random() * 101);
+	}
+    function dispatchAccept() {
+		dispatch('decision', 'Accepted');
+	}
+    function dispatchReject() {
+		dispatch('decision', 'Rejected');
 	}
 </script>
 
 <div class="">
     <div class="title"> Interview Result: {getRandomIntBetweenZeroAndOneHundred()}% </div>
     <div class="flex ">
-        <button class="btn bg-tertiary-500">Accept</button>
-        <button class="btn bg-primary-500">Reject</button>
+        <button class="btn bg-tertiary-500" on:click={dispatchAccept}>Accept</button>
+        <button class="btn bg-primary-500" on:click={dispatchReject}>Reject</button>
     </div>
     
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,18 @@
 <script>
 	import CustomAvatar from '../components/CustomAvatar.svelte';
 	import ResultAndDecision from '../components/ResultAndDecision.svelte';
+	import ComponentEvent from '../components/ComponentEvent.svelte';
 
 	let currentView = 0;
+	let decision = "";
+	let components = [{ listeners: { decision: handleMessage }, props: {}, component: ResultAndDecision }];
+	// let rad = { listeners: { decision: handleMessage }, props: {}, component: ResultAndDecision };
 	function changeCurrentViewToOne() {
 		currentView = 1
+	}
+	function handleMessage(event) {
+		decision = event.detail;
+		console.log(decision);
 	}
 </script>
 <div class="container mx-auto p-8 space-y-8">
@@ -24,7 +32,16 @@
 	
 	<div class="flex"> 
 		<div class="flex justify-center items-center card card-hover p-0 h-72 w-72 mr-6" on:click={changeCurrentViewToOne} on:keypress={changeCurrentViewToOne}>
-			<svelte:component this={currentView == 0? CustomAvatar : ResultAndDecision}></svelte:component>
+			<!-- Got from:
+				https://svelte.dev/repl/5b495a6d61e64d0cabdb3657f100837c?version=3.18.2  
+			No idea why it doesn't work using component[0], though.-->
+			{#if currentView === 0}
+				<CustomAvatar id={0} />
+			{:else}
+				{#each components as component}
+					<ComponentEvent {component}/>
+				{/each}
+			{/if}
 		</div>
 	</div>
 </div>
