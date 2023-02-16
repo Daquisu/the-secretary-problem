9a5dd87d371555a7b9641c2428957deb13efd017
Vinicius Akira Imaizumi <daquisu@gmail.com>
2023-02-16 10:50:42 +0100

    Remove option of refusing same person / accepting refused candidates

--- src/components/ResultAndDecision.svelte
+++ src/components/ResultAndDecision.svelte
@@ -2,23 +2,32 @@
 	import { createEventDispatcher } from 'svelte';
 	export let skill;
 	export let is_last;
+	let already_clicked = false;
 
 	const dispatch = createEventDispatcher();
 
 	function dispatchAccept() {
-		dispatch('decision', 'Accepted');
+		if (!already_clicked) {
+			dispatch('decision', 'Accepted');
+			already_clicked = true;
+		}
 	}
 	function dispatchReject() {
-		dispatch('decision', 'Rejected');
+		if (!already_clicked) {
+			dispatch('decision', 'Rejected');
+			already_clicked = true;
+		}
 	}
 </script>
 
 <div class="w-72 h-72 justify-center items-center flex flex-col">
-	<div>Interview Result: {skill}%</div>
-	<div>
-		<button class="btn bg-tertiary-500" on:click={dispatchAccept}>Accept</button>
-		{#if !is_last}
-			<button class="btn bg-primary-500" on:click={dispatchReject}>Reject</button>
-		{/if}
+	<div class="w-48 h-48 flex flex-col justify-between items-center">
+		<h2>Interview Result: {skill}%</h2>
+		<div>
+			<button class="btn bg-tertiary-500" on:click={dispatchAccept}>Accept</button>
+			{#if !is_last}
+				<button class="btn bg-primary-500" on:click={dispatchReject}>Reject</button>
+			{/if}
+		</div>
 	</div>
 </div>
